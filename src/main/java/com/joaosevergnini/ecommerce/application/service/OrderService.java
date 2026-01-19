bfbd4d185c418b58b388a8af648e595fb578da3a
Joao-Severgnini <joaoramm@gmail.com>
2026-01-19 13:11:00 -0300

    Add methods: findOrderById, findOrdersByCustomerId in OrderService

--- src/main/java/com/joaosevergnini/ecommerce/application/service/OrderService.java
+++ src/main/java/com/joaosevergnini/ecommerce/application/service/OrderService.java
@@ -7,6 +7,9 @@ import com.joaosevergnini.ecommerce.infrastructure.pesistence.connection.Databas
 import com.joaosevergnini.ecommerce.infrastructure.pesistence.repository.*;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 public class OrderService {
     private final OrderRepository orderRepository;
@@ -26,8 +29,11 @@ public class OrderService {
         this.customerRepository = customerRepository;
     }
 
-    public Order CreateOrder(Order order){
-        try (Connection conn = DatabaseConnection.getConnection()) {
+    public Order createOrder(Order order){
+        Connection conn = null;
+
+        try {
+            conn = DatabaseConnection.getConnection();
             conn.setAutoCommit(false);
 
             customerRepository.findById(conn, order.getCustomerId())
@@ -53,7 +59,58 @@ public class OrderService {
             return savedOrder;
 
         }catch (Exception e){
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    throw new RuntimeException("Rollback failed", ex);
+                }
+            }
             throw new RuntimeException("Error creating order", e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException("Error closing connection", e);
+                }
+            }
+        }
+    }
+
+    public Optional<Order> findOrderById(Long orderId) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+
+            Optional<Order> orderOpt = orderRepository.finbyId(conn, orderId);
+            if (orderOpt.isEmpty()) {
+                return  Optional.empty();
+            }
+
+            Order order = orderOpt.get();
+
+            orderItemRepository.findByOrderId(conn, order.getId())
+                    .forEach(order::addItem);
+
+            return Optional.of(order);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error finding order", e);
+        }
+    }
+
+    public List<Order> findByCustomerId(Long customerId) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+
+            List<Order> orders = orderRepository.findByCustomerId(conn, customerId);
+
+            for (Order order : orders) {
+                orderItemRepository.findByOrderId(conn, order.getId())
+                        .forEach(order::addItem);
+            }
+
+            return orders;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error finding orders by customer ID", e);
         }
     }
 
